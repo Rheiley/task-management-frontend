eaf6ac4c4625237acf70615339cc088f9b31a347
Rheiley <118871025+Rheiley@users.noreply.github.com>
2023-11-28 15:00:12 -0800

    Add description attribute

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,54 +1,83 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState("")
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, { text: newTask.trim(), description: "", checked: false }]);
+      setNewTask("");
+    }
+  };
 
-    const handleAddTask = () => {
-        if(newTask.trim() !== ""){
-            setTasks([...tasks, {text: newTask.trim(), checked: false}]);
-            setNewTask("");
-        } 
-    };
+  const handleDeleteTask = (id) => {
+    const newTasks = [...tasks];
+    newTasks.splice(id, 1);
+    setTasks(newTasks);
+  };
 
-    const handleDeleteTask = (id) => {
-        const newTasks = [...tasks];
-        
-        newTasks.splice(id, 1);
-        setTasks(newTasks);
-    };
+  const handleToggleTask = (id) => {
+    const newTasks = [...tasks];
+    newTasks[id].checked = !newTasks[id].checked;
+    setTasks(newTasks);
+  };
 
-    const handleToggleTask = (id) => {
-        const newTasks = [...tasks];
-        newTasks[id].checked = !newTasks[id].checked;
-        setTasks(newTasks);
-    };
+  const handleDescriptionChange = (id, description) => {
+    const newTasks = [...tasks];
+    newTasks[id].description = description;
+    setTasks(newTasks);
+  };
 
-    return(
-        <div>
-            <h1 class="title">To-Do List</h1>
-            <section class="main">
-            <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
-            <button onClick={(handleAddTask)}>Add</button>
-            <ul class="list">
-                {tasks.map((task, id) => (
-                    <li key={id} style={{display: "flex"}}>
-                        <div style = {{display: "flex", alignItems: "center"}}>
-                            <input 
-                                type="checkbox" 
-                                checked={task.checked} 
-                                onChange={() => handleToggleTask(id)}
-                            />
-                            <span style={{marginRight: "10px", textDecoration: task.checked ? "line-through" : "none"}}>{task.text}</span>
-                            <button style={{marginTop: "5px", marginBottom: "5px"}} onClick={() => handleDeleteTask(id)}>Delete</button>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            </section>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="title">To-Do List</h1>
+      <section className="main">
+        <input
+          type="text"
+          placeholder="Task name"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add</button>
+        <ul className="list">
+          {tasks.map((task, id) => (
+            <li key={id} style={{ display: "flex" }}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <input
+                  type="checkbox"
+                  checked={task.checked}
+                  onChange={() => handleToggleTask(id)}
+                />
+                <span
+                  style={{
+                    marginRight: "10px",
+                    textDecoration: task.checked ? "line-through" : "none",
+                  }}
+                >
+                  {task.text}
+                </span>
+                <input
+                  class="desc"  
+                  type="text"
+                  placeholder="Description"
+                  value={task.description}
+                  onChange={(e) => handleDescriptionChange(id, e.target.value)}
+                />
+                <button
+                  style={{ marginTop: "5px", marginBottom: "5px" }}
+                  onClick={() => handleDeleteTask(id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default TodoList;
